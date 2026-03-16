1dc0744477047c4eee2dcfa84527fe6fc41a18b4
TE24C-Vide-Bultmark <vide.bultmark@elev.ga.ntig.se>
2026-03-16 09:39:59 +0100

    gjort om några while-loopar till for-loopar

--- Slutprojekt/Toolbox.cs
+++ Slutprojekt/Toolbox.cs
@@ -3,8 +3,10 @@ public class Toolbox
     // introducerar spelet och låter spelaren namnge staden samt grundaren
     public static string Intro(List<string> people)
     {
+        // förklarar spelet
         Console.WriteLine("Hello and welcome to my game!");
         Console.WriteLine("In this game you manage a city and watch it grow.");
+        // spelaren får välja några namn
         Console.WriteLine("Please choose a name for your city.");
         string cityname = Console.ReadLine();
         Console.WriteLine($"And who are the two founders of {cityname}?");
@@ -23,12 +25,11 @@ public class Toolbox
     public static void DisplayBuildqueue(List<Building> buildqueue)
     {
         Console.WriteLine($"building - {buildqueue[0].name} ({buildqueue[0].progress}/{buildqueue[0].costAmount} {buildqueue[0].costResource.name})");
-
-        int iteration = 1;
-        while (iteration < buildqueue.Count)
+        // loopen går genom alla byggnader spelaren kan spela
+        for (int iteration = 1; iteration < buildqueue.Count; iteration++)
         {
+            // skriver upp byggnadens plats, namn, progress, kostnad och vilken resurs den kostar
             Console.WriteLine($"{iteration}) {buildqueue[iteration].name} ({buildqueue[iteration].progress}/{buildqueue[iteration].costAmount} {buildqueue[iteration].costResource.name})");
-            iteration++;
         }
 
     }
@@ -42,11 +43,10 @@ public class Toolbox
     {
         Console.WriteLine("day " + day);
         Console.WriteLine("food needed until next person: " + people.Count * people.Count);
-        int iteration = 0;
-        while (iteration < resources.Count)
+        // skriver upp alla resurser spelaren kan producera
+        for (int iteration = 0; iteration < resources.Count; iteration++)
         {
             Console.WriteLine(resources[iteration].name + ": " + resources[iteration].amount);
-            iteration++;
         }
     }
 
@@ -57,19 +57,21 @@ public class Toolbox
     // skriver upp personer och arbetsuppgifter
     public static void DisplayWork(List<string> people, List<Building> buildings)
     {
-        int iteration = people.Count - 1;
-        while (iteration >= 0)
+        // går genom all personer från nyast till äldst
+        for (int iteration = people.Count - 1; iteration >= 0; iteration--)
         {
+            // skriver namn
             Console.Write(people[iteration]);
+            // om det inte finns någon byggnad att jobba i kommer de bygga nya byggnader
             if (iteration >= buildings.Count)
             {
                 Console.WriteLine(" - building");
             }
+            // om det finns en byggnad kommer de att jobba i den
             else
             {
                 Console.WriteLine(" - " + buildings[iteration].name);
             }
-            iteration--;
         }
     }
 
@@ -81,7 +83,9 @@ public class Toolbox
     public static bool SwitchBuilding(List<Building> buildqueue)
     {
         int input;
+        // sätter in det skrivna numret i input
         int.TryParse(Console.ReadLine(), out input);
+        // om spelarens input korresponderar till en av byggvalen börjar staden bygga den byggnaden
         if (buildqueue.Count > input && input > 0)
         {
             Building temp = buildqueue[input];
@@ -89,6 +93,7 @@ public class Toolbox
             buildqueue[0] = temp;
             return false;
         }
+        // om inputen inte korresponderar till en byggnad tolkas detta som en pass
         else return true;
     }
 
@@ -99,23 +104,23 @@ public class Toolbox
     public static void Produce(List<Resource> resources, List<Building> buildings, List<string> people)
     {
         // loop som går igenom varje resurs
-        int iterationResource = 0;
-        while (iterationResource < resources.Count)
+        for (int iterationResource = 0; iterationResource < resources.Count; iterationResource++)
         {
-            int iterationBuilding = 0;
-            while (iterationBuilding < buildings.Count)
+            // looå som går genom varje byggnad
+            for (int iterationBuilding = 0; iterationBuilding < buildings.Count; iterationBuilding++)
             {
                 // kollar efter byggnader som producerar en viss resurs och ökar produktionen av den resursen med så mycket byggnaden producerar
                 if (buildings[iterationBuilding].productionResource == resources[iterationResource])
                 {
+                    // producerar den resursen
                     resources[iterationResource].amount += buildings[iterationBuilding].productionAmount;
                 }
-                iterationBuilding++;
             }
-            iterationResource++;
         }
         // produktion ej baserat på byggnader
+        // rsources[0] är food
         resources[0].amount -= people.Count;
+        // resources[2] är science
         resources[2].amount += people.Count;
     }
 
@@ -130,14 +135,8 @@ public class Toolbox
         {
             food.amount = 0;
             Console.Clear();
-            Console.WriteLine("congratulations your a new member of your city has appeared!");
-            string name = "";
-            while (name.Length < 1 || name.Length > 20)
-            {
-                Console.WriteLine("please choose a name for the member. Name must be between 1 and 20 characters");
-                name = Console.ReadLine();
-            }
-            people.Add(name);
+            Console.WriteLine("congratulations, a new member of your city has appeared!");
+            AddPerson(people);
         }
     }
 
@@ -197,20 +196,43 @@ public class Toolbox
 
 
 
+    // denna metod ser till att du alltid har de resurser du kan producera i den resurslista
     public static void AddResource(List<Resource> resources, List<Building> techOptions, int choice)
     {
-        int itertion = 0;
-        while (itertion < resources.Count)
+        // loop som körs för varje resurs
+        for (int i=0; i < resources.Count; i++)
         {
-            if (resources[itertion] == techOptions[choice - 1].productionResource)
+            // om resursen redan finns bryts loopen
+            if (resources[i] == techOptions[choice - 1].productionResource)
             {
                 break;
             }
-            else if (itertion == resources.Count - 1)
+            // om resursen inte finns läggs den till i resurser
+            else if (i == resources.Count - 1)
             {
                 resources.Add(techOptions[choice - 1].productionResource);
             }
-            itertion++;
         }
+    }
+
+
+
+
+
+    // låter spelaren lägga till en person i staden
+    public static void AddPerson(List<string> people)
+    {
+        // sätter namnet till ett otillåtet värde med syfte att köra loopen
+        string name = "";
+        // loopen kollar efter otillåtna värden
+            while (name.Length < 1 || name.Length > 20)
+            {
+                // informerar om restriktioner
+                Console.WriteLine("please choose a name for the new person. Name must be between 1 and 20 characters");
+                // lägger in det skrivna namnet i en variabel
+                name = Console.ReadLine();
+            }
+            // lägger till det skrivna namnet in i staden
+            people.Add(name);
     }
 }
