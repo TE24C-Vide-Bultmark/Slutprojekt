2ededac5dd3e058824b2394f94c2e3f1d6713be4
TE24C-Vide-Bultmark <vide.bultmark@elev.ga.ntig.se>
2026-03-20 13:20:21 +0100

    metal!

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -2,18 +2,19 @@
 // jag använder listor istället för arrayer då jag vill att programmet ska kunna lägga till objekt i de under programmets gång, detta gäller för alla listor i programmet
 
 // resurserna i spelet
-
 Resource food = new Resource() { name = "Food" };
 Resource wood = new Resource() { name = "Wood" };
 Resource science = new Resource() { name = "Science" };
+// lista med de reurser du har tillgång till
+List<Resource> resources = [food, wood, science];
 Resource stone = new Resource() { name = "Stone" };
 Resource ore = new Resource() { name = "Ore" };
-Resource metal = new Resource() { name = "metal" };
-List<Resource> resources = [food, wood, science];
+Resource metal = new Resource() { name = "Metal" };
 
 // byggnaderna i spelet
 Building farm = new Building() { name = "Farm", costResource = wood, production = new() {{food, 3}}};
 Building carpentry = new Building() { name = "Carpentry", costAmount = 5, costResource = wood , production = new() {{wood, 1}}};
+// byggnaderna du kan bygga
 List<Building> buildingOptions = [farm, carpentry];
 Building bigFarm = new Building() { name = "Big Farm", costAmount = 30, costResource = stone, production = new() {{food, 10}} };
 Building library = new Building() { name = "Library", costAmount = 3, costResource = wood, production = new() {{science, 1}} };
@@ -21,18 +22,22 @@ Building quarry = new Building() { name = "Quarry", costAmount = 15, costResourc
 Building sawmill = new Building() { name = "Sawmill", costAmount = 10, costResource = wood, production = new() {{wood, 2}} };
 Building badMine = new Building() { name = "Bad Mine", costAmount = 40, costResource = wood, production = new() {{ore, 1}} };
 Building goodMine = new Building() { name = "Good Mine", costAmount = 15, costResource = stone, production = new() {{ore, 1}, {stone, 1}} };
-Building forge = new Building() {};
+Building forge = new Building() { name = "Forge", costAmount = 25, costResource = stone, production = new() {{ore, -1}, {metal, 1}} };
+
+// teknologier, det ska kunna försvinna byggnader från dessa, därav måste de vara listor istället för arrayer
 List<List<Building>> technologytree = [];
 List<Building> technologies0 = [library, quarry, quarry, sawmill];
 technologytree.Add(technologies0);
 List<Building> technologies1 = [bigFarm, library, quarry, badMine];
 technologytree.Add(technologies1);
-List<Building> technologies2 = [bigFarm, bigFarm, badMine, goodMine];
+List<Building> technologies2 = [bigFarm, bigFarm, badMine, goodMine, forge];
 technologytree.Add(technologies2);
 List<Building> techOptions = [];
 
+// lista som lagrar alla personer i din stad
 List<string> people = [];
 
+// lista med alla byggnader du har byggt
 List<Building> buildings = [farm, carpentry];
 
 string cityname = Toolbox.Intro(people);
@@ -54,7 +59,7 @@ while (true)
 
     Console.WriteLine("\n\n--------------------------------------------------------------------------------");
     Console.WriteLine("Press Enter to go to the next day");
-    Console.WriteLine("Enter the number next to the building you want to build to start building it");
+    Console.WriteLine("Enter the number next to the building you want to build");
 
     // spelaren får möjlighet att byta vilken byggnad som byggs
     if (Toolbox.SwitchBuilding(buildingOptions))
